1e75db4299febdf2d5cb2d1612b2a9a646cf2766
faisal@doku.com <faisal@doku.com>
2022-12-12 20:16:04 +0700

    try catch

--- routes/artikel.js
+++ routes/artikel.js
@@ -9,28 +9,42 @@ const ArtikelModel = require('../model/artikel')
 router.post('/',   async(req, res) =>{
     const { author, title, body } = req.body
 
+
+    try{
     const artikel = await ArtikelModel.create({
         author, title, body 
       });
 
-    res.status(200).json({
-        data: artikel,
-        metadata: "berhasil di tambahkan"
-      })
+ 
+        res.status(200).json({
+            data: artikel,
+            metadata: "berhasil di tambahkan"
+          })
+    }catch(e){  
+        res.status(400).json({
+          error: "salah data "
+        })
+      }
+    
 
 })
-
 
 router.get('/', cache("10 minutes"), async(req, res) => {
     const { author } = req.body
    
     const authorFind = await ArtikelModel.findOne({where : { author :author}})
 
-
+    try{
     res.status(200).json({
         res : authorFind.author,
         req : authorFind.title
     })
+    }catch(e){  
+        res.status(400).json({
+          error: "data not found"
+        })
+      }
+
 })
 
 
